548cf28398568cd1d18ec4482d80911b29aa2393
meghanar2205 <131662069+meghanar2205@users.noreply.github.com>
2024-03-04 14:19:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+// App.js
+import React from 'react';
+import MainForm from './Components/MainForm'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='cont'>
+      <MainForm />
+    </div>
+  );
+};
+
+export default App;
